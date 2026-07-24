83543e124468c091f51869146b990cea55d87798
Prakash9862 <v.bridet98@gmail.com>
2026-07-24 21:57:49 +0200

    feat(ui): load Hanuman OS styles

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App'
 import './styles.css'
 import './wikipedia.css'
+import './hanuman-os.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
