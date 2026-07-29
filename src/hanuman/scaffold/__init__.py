e00258e97eadce658502a4106e61797673b4f177
Prakash9862 <v.bridet98@gmail.com>
2026-07-30 00:26:04 +0200

    feat(scaffold): add connector scaffold package

---
+++ src/hanuman/scaffold/__init__.py
@@ -0,0 +1,9 @@
+from hanuman.scaffold.connector import ConnectorScaffold, ScaffoldPlan
+from hanuman.scaffold.manifest import ConnectorManifest, load_connector_manifest
+
+__all__ = [
+    "ConnectorManifest",
+    "ConnectorScaffold",
+    "ScaffoldPlan",
+    "load_connector_manifest",
+]
